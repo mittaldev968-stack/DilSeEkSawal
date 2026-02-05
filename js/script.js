8822c5f65b0f04defd2234a9f9603856db7434b8
mittaldev968-stack <mittaldev968@gmail.com>
2026-02-06 03:25:09 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function goYes() {
+  document.body.style.opacity = "0";
+  setTimeout(() => {
+    window.location.href = "yes.html";
+  }, 600);
+}
+
+function goNo() {
+  document.body.style.opacity = "0";
+  setTimeout(() => {
+    window.location.href = "no.html";
+  }, 600);
+}
